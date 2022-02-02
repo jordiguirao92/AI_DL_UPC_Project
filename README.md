f1cd68a0dca6a1b0d70564c5dc2c2f4a376211df
jordiguirao92 <jordiguirao92@users.noreply.github.com>
2022-02-02 21:26:12 +0100

    WIP

--- README.md
+++ README.md
@@ -61,3 +61,21 @@ class noiseLayer_normal(nn.Module):
 ## Como entrenar:
 - Optimizar L1 loss (o cualquier metrica que calcule la diferencia entre la predicción "limpia" y la imagen limpia de verdad)
 - GANs son bastante populares: L1 loss para el generador + la loss del discriminador multiplicada por algun factor de escala
+
+
+- Preparar justificacion de porque no usamor ruido artificial
+- PixtoPix y UNET (Generador, quitar el ruido) Quitar ruido con UNET
+- Tener datos descargados
+- Implementar dataset y dataloader (Imagen y ground truth en los path)
+- Dataset: https://github.com/csjunxu/PolyU-Real-World-Noisy-Images-Dataset
+
+1. Script para descargar las imagenes https://www.eecs.yorku.ca/~kamel/sidd/dataset.php
+2. Tener imagenes
+
+
+# Useful links:
+- https://github.com/csjunxu/PolyU-Real-World-Noisy-Images-Dataset/tree/master/CroppedImages
+- https://github.com/jaxony/unet-pytorch/blob/master/model.py
+- https://www.pyimagesearch.com/2021/11/08/u-net-training-image-segmentation-models-in-pytorch/
+- https://github.com/milesial/Pytorch-UNet/blob/master/unet/unet_parts.py
+- https://github.com/yinhaoz/denoising-fluorescence/blob/d83cad96b205793da0f62f6dc8094799d61929e6/denoising/train_n2n.py#L15
--- model/model.py
+++ model/model.py
@@ -3,28 +3,106 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-# TODO Define the model
+def conv2d(inChannels, outChannels, kernel_size=3, padding=1, bias=False):
+    return nn.Conv2d(inChannels, outChannels, kernel_size=kernel_size, padding=padding, bias=bias)
 
-class MyModel(nn.Module):
+def relu():
+    return nn.ReLU()
 
-    def __init__(self, h1=32, h2=64, h3=128, h4=256, h5=256):
-        super().__init__()
-        self.conv1 = nn.Conv2d(3, h1, 3, padding=1)
-        self.conv2 = nn.Conv2d(h1, h2, 3, padding=1)
-        self.conv3 = nn.Conv2d(h2, h3, 3, padding=1)
-        self.conv4 = nn.Conv2d(h3, h4, 3, padding=1)
+def maxPool2d(kernel_size=2):
+    return nn.MaxPool2d(kernel_size=kernel_size)
+    
+def convTranspose2d(in_channels, out_channels, kernel_size=2, stride=1, padding=0):
+    return nn.ConvTranspose2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding)
 
-        self.fc1 = nn.Linear(8*8*h4, h5)
-        self.fc2 = nn.Linear(h5, 1)
+class BlockDown(nn.Module):
+    def __init__(self, inChannels, outChannels):
+        super().__init__
+        self.inChannels = inChannels
+        self.outChannels = outChannels
 
-        self.pool = nn.MaxPool2d(2)
+        self.conv1 = conv2d(self.inChannels, self.outChannels)
+        self.conv2 = conv2d(self.inChannels, self.outChannels)
+        self.relu = relu()
+        self.pool = maxPool2d()
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
-        x = self.pool(F.relu(self.conv4(x)))
-        x = torch.flatten(x, start_dim=1)
+        return self.pool(self.relu(self.conv2(self.conv1(x))))
 
-        x = F.relu(self.fc1(x))
-        return self.fc2(x)
+
+class BlockUp(nn.Module):
+    def __init__(self, inChannels, outChannels):
+        super().__init__
+        self.inChannels = inChannels
+        self.outChannels = outChannels
+
+        self.conv1 = conv2d(self.inChannels, self.outChannels)
+        self.conv2 = conv2d(self.inChannels, self.outChannels)
+        self.relu = relu()
+        self.pool = convTranspose2d(self.inChannels, self.outChannels)
+
+    def forward(self, x):
+        return self.pool(self.relu(self.conv2(self.conv1(x))))
+
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super().__init__
+        self.block1 = BlockDown(3, 32)
+        self.block2 = BlockDown(32, 64)
+        self.block3 = BlockDown(64, 128)
+        self.block4 = BlockDown(128, 256)
+        self.block5 = BlockDown(256, 512)
+
+    def forward(self, x):
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.block5(x)
+        return x
+
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super().__init__
+        self.block1 = BlockUp(512, 1024)
+        self.block2 = BlockUp(1024, 512)
+        self.block3 = BlockUp(512, 256)
+        self.block4 = BlockUp(256, 128)
+        self.block5 = BlockUp(128, 64)
+
+    def forward(self, x):
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.block5(x)
+        return x
+
+class Output(nn.Module):
+    def __init__(self, inChannels, outChannels):
+        super().__init__
+        self.inChannels = inChannels
+        self.outChannels = outChannels
+
+        self.conv1 = conv2d(64, 32)
+        self.conv2 = conv2d(32, 2, kernel_size=1)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        return x
+
+class GeneratorUnet(nn.Module):
+    def __init__(self):
+        super().__init__
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+        self.output = Output()
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        x = self.output(x)
+        return x
